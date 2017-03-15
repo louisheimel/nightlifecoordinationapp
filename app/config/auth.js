48b58a9be2906e9a4c1715ef13b469697b5244c4
Unknown Name <louis.heimel@mines.sdsmt.edu>
2017-03-15 20:04:53 +0000

    edit twitter app settings

--- app/config/auth.js
+++ app/config/auth.js
@@ -3,7 +3,7 @@
 module.exports = {
 	'twitterAuth': {
 		'consumerSecret': 'T16dMAdjQVdsocxy9poqjTDpYxTpQ17b3GKtgzb0ugzhcTtLsn',
-		'callbackURL': 'https://sleepy-atoll-68387.herokuapp.com//auth/twitter/callback',
+		'callbackURL': 'https://sleepy-atoll-68387.herokuapp.com/auth/twitter/callback',
 		'consumerKey': 'tpO1QHH8DPM2zECMLIX9fByV4',
 	}
 };
